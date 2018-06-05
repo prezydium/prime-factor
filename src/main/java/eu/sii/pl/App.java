e6c1c7a4f6641427972c9784ac9841fe399ee1d1
Krzysztof Syska <ksyska@pl.sii.eu>
2018-06-05 15:17:11 +0200

    Added exit option

--- src/main/java/eu/sii/pl/App.java
+++ src/main/java/eu/sii/pl/App.java
@@ -1,15 +1,30 @@
 package eu.sii.pl;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Enter non-negative integer to see its prime factors: ");
-        PrimeFactorize primeFactorize = new PrimeFactorize();
-        Scanner scanner = new Scanner(System.in);
-        int numberToGetPrimeFactors = scanner.nextInt();
-        List<Integer> list = primeFactorize.getPrimeFactors(numberToGetPrimeFactors);
-        list.forEach(System.out::println);
+
+    private final Logger LOG = Logger.getLogger(App.class.toString());
+
+    int numberToGetPrimeFactors;
+
+    public void process() {
+        do {
+            System.out.println("Enter non-negative integer to see its prime factors, or 0 to exit: ");
+            PrimeFactorize primeFactorize = new PrimeFactorize();
+            Scanner scanner = new Scanner(System.in);
+            try {
+                numberToGetPrimeFactors = scanner.nextInt();
+            } catch (InputMismatchException | IllegalArgumentException e) {
+                LOG.log(Level.INFO, "entered value is not a positive java integer");
+            }
+            List<Integer> list = primeFactorize.getPrimeFactors(numberToGetPrimeFactors);
+            list.forEach(System.out::println);
+        } while (numberToGetPrimeFactors != 0);
     }
 }
---
+++ src/main/java/eu/sii/pl/Main.java
@@ -0,0 +1,8 @@
+package eu.sii.pl;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new App().process();
+    }
+}
